3207ca94a9dbcf1c9194d91b2b5a1388f21907e9
karel padilla <111618315+karadelacra@users.noreply.github.com>
2024-06-16 11:51:40 -0600

    Agregando los metadatos de los productos predeterminados

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 const poke_container = document.getElementById('poke-container')
 const pokemon_count = 150
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    main: '#FDDFDF',
+    mexican: '#DEFDE0'
 }
 
 const main_types = Object.keys(colors)
 
-const fetchPokemons = async () => {
-    for(let i = 1; i <= pokemon_count; i++) {
+const fetchPokemons = async() => {
+    for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
 }
 
-const getPokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemon = async(id) => {
+    const url = `Rancheritos.com/${id}.json`
     const res = await fetch(url)
     const data = await res.json()
     createPokemonCard(data)
@@ -47,7 +35,7 @@ const createPokemonCard = (pokemon) => {
 
     const pokemonInnerHTML = `
     <div class="img-container">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${name}">
+        <img src="${pokemon.image}" alt="${name}">
     </div>
     <div class="info">
         <span class="number">#${id}</span>
