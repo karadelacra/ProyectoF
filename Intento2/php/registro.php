33d914c4f0db228a3b68c7899c38584fcb0c0231
vec70r <lizan.2001@gmail.com>
2024-06-22 20:28:35 -0600

    commit

--- Intento2/php/registro.php
+++ Intento2/php/registro.php
@@ -20,10 +20,10 @@ $telefono = $_POST['nTelefono'];
 $sql = "INSERT INTO usuarios (nombre, apellido, username, num_boleta, correo, contraseña, horario_inicio, horario_fin, tarjeta, clabe_interbancaria, telefono) 
             VALUES ('$nombre', '$apellido', '$nombre_usuario', '$numero_boleta', '$correo', '$password', '$hora_inicio', '$hora_fin', '$tarjeta', '$clabe_interbancaria', '$telefono')";
 
-if ($conexion->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {
     echo "Registro exitoso.";
 } else {
-    echo "Error: " . $sql . "<br>" . $conexion->error;
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
 // Cerrar conexión
