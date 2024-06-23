ad613b34a10b58a17eab4c541350a436bbc4b04d
vec70r <lizan.2001@gmail.com>
2024-06-22 18:49:22 -0600

    corección en pagina de registro

--- Intento2/php/registro.php
+++ Intento2/php/registro.php
@@ -17,7 +17,7 @@ $clabe_interbancaria = $_POST['cc-number'];
 $telefono = $_POST['nTelefono'];
 
 // Preparar consulta SQL para insertar datos en la tabla Usuarios
-$sql = "INSERT INTO Usuarios (nombre, apellido, username, num_boleta, correo, contraseña, horario_inicio, horario_fin, tarjeta, clabe_interbancaria, telefono) 
+$sql = "INSERT INTO usuarios (nombre, apellido, username, num_boleta, correo, contraseña, horario_inicio, horario_fin, tarjeta, clabe_interbancaria, telefono) 
             VALUES ('$nombre', '$apellido', '$nombre_usuario', '$numero_boleta', '$correo', '$password', '$hora_inicio', '$hora_fin', '$tarjeta', '$clabe_interbancaria', '$telefono')";
 
 if ($conexion->query($sql) === TRUE) {
@@ -27,6 +27,6 @@ if ($conexion->query($sql) === TRUE) {
 }
 
 // Cerrar conexión
-$conexion->close();
+$conn->close();
 
 ?>
